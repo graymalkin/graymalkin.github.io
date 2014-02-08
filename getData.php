2ff39d8289d15936b9ca966ea85ee8caaea1b56c
Simon Cooksey <sjc80@kent.ac.uk>
2014-02-08 01:25:34 +0000

    Rescaled graphs to showoff my github account a little better

--- getData.php
+++ getData.php
@@ -2,14 +2,14 @@
 $filename = "graymalkin.json";
 if(time()-filemtime($filename) > 12 * 3600 || isset($_GET['update']))
 {
-// The file is over 12 hours old, we should get it again, then echo it's contents
-shell_exec("wget http://osrc.dfm.io/graymalkin.json -O /var/www/graymalkin.json &");
-echo file_get_contents("graymalkin.json");
+	// The file is over 12 hours old, we should get it again, then echo it's contents
+	shell_exec("wget http://osrc.dfm.io/graymalkin.json -r -O /var/www/graymalkin.json &");
+	echo file_get_contents("graymalkin.json");
 }
 else
 {
-// The file is less than 12 hours old, let's just echo out the local cache
-echo file_get_contents("graymalkin.json");
+	// The file is less than 12 hours old, let's just echo out the local cache
+	echo file_get_contents("graymalkin.json");
 }
 
 ?>
